96c47ebf4401551f61c04ac584b10e7606f7c2ad
Gourav Tiwary <90273558+printgourav@users.noreply.github.com>
2025-06-01 17:00:05 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+
+const msg = new SpeechSynthesisUtterance();
+let voices = [];
+const voicesDropdown = document.querySelector('[name="voice"]');
+const options = document.querySelectorAll('[type="range"], [name="text"]');
+const speakButton = document.querySelector('#speak');
+const stopButton = document.querySelector('#stop');
+
+function populateVoices(){
+    voices = this.getVoices();
+    voicesDropdown.innerHTML = voices
+    .filter(voice => voice.lang.includes('en-IN'))
+    .map(voice => `<option value "${voices.name}">${voice.name} (${voice.lang})</option`)
+    .join('');
+}
+
+function setVoice() {
+    msg.voice = voices.find(voice => voice.name === this.value);
+    toggle();
+}
+
+function toggle(startOver = true){
+msg.text = document.querySelector('[name="text"]').value;
+    speechSynthesis.cancel();
+    if(startOver){
+        speechSynthesis.speak(msg);
+    }
+}
+
+function setOption(){
+    console.log(this.name, this.value);
+    msg[this.name] = this.value;
+    toggle();
+}
+
+speechSynthesis.addEventListener('voiceschanged',populateVoices)
+voicesDropdown.addEventListener('change', setVoice);
+options.forEach(option => option.addEventListener('chnage', setOption));
+speakButton.addEventListener('click', toggle);
+stopButton.addEventListener('click', () => toggle(false))
+
+
